2ac975d5cb6d5fb998b943c693770df279861a32
shermar <shermar.vander.haeghe@student.howest.be>
2019-12-05 09:22:06 +0100

    added field and added players into the initialize

--- src/main/java/ui/fx/ShowPlayersController.java
+++ src/main/java/ui/fx/ShowPlayersController.java
@@ -1,4 +1,29 @@
 package ui.fx;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.ResourceBundle;
+
+import data.Repo;
+import domain.Player;
+import javafx.fxml.FXML;
+import javafx.scene.control.TextArea;
 
 public class ShowPlayersController {
+    private List<Player> players = new ArrayList<>();
+
+    @FXML
+    private ResourceBundle resources;
+
+    @FXML
+    private URL location;
+
+    @FXML
+    private TextArea ta_allPlayers;
+
+    @FXML
+    void initialize() {
+        assert ta_allPlayers != null : "fx:id=\"ta_allPlayers\" was not injected: check your FXML file 'Untitled'.";
+        players = Repo.getPlayerRepo().getPlayers();
+    }
 }
