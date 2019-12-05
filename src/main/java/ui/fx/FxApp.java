7ffe74f34ab5ed8b0ba0113ba012c651105b5078
shermar <shermar.vander.haeghe@student.howest.be>
2019-12-05 09:50:49 +0100

    added path in start

--- src/main/java/ui/fx/FxApp.java
+++ src/main/java/ui/fx/FxApp.java
@@ -15,7 +15,7 @@ public class FxApp extends Application{
 
         @Override
         public void start(Stage primaryStage) throws IOException {
-            Parent root = FXMLLoader.load(getClass().getResource("/fxml/..."));
+            Parent root = FXMLLoader.load(getClass().getResource("/fxml/Show"));
             Scene scene = new Scene(root);
             primaryStage.setScene(scene);
             primaryStage.show();
